a190c8ab45dbfbe6858515fee18c9031d29eabce
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-10-22 22:16:04 -0300

    Implement profile-tab component with code for testing imports

--- src/app/components/profile-tab/profile-tab.component.ts
+++ src/app/components/profile-tab/profile-tab.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
-  selector: 'app-profile-tab',
-  templateUrl: './profile-tab.component.html',
-  styleUrls: ['./profile-tab.component.css']
+	selector: 'app-profile-tab',
+	templateUrl: './profile-tab.component.html',
+	styleUrls: ['./profile-tab.component.css']
 })
 export class ProfileTabComponent {
 
+	@Output() updateProfileInfoRequest = new EventEmitter();
+	@Input() dataUrl: string = '';
+	@Input() profileUser: string = '';
+
+	fetchUserData(dataUrl: string) {
+		const context = require.context('../myModules', false, /\.json$/);
+		// console.log(data)
+		// let data = "./gabriel.json";
+		// import("../../../assets/people-info/alanis.json").then(({ default: x }) => console.log(x))
+		// let userDataPromise = import(dataUrl);
+		// console.log(userDataPromise)
+		// this.updateProfileInfoRequest.emit(userDataPromise);
+		import(dataUrl).then(x => console.log(x))
+	}
 }
