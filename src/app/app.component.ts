1d682773e1c053313baadbc0003a676208e0d12a
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-10-22 22:17:49 -0300

    Implement app.component for testing component profile-tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'final_project';
+
+	title = 'final_project';
+	content: any = 'loading...'
+	curProfileUser: string = 'default'
+
+	// ngOnInit() {
+	// 	 require.context('../myModules', false, /\.json$/);
+	// }
+
+	async loadNewContent(newContentPromise: Promise<any>) {
+		console.log('valor inicial de newContent = ', newContentPromise)
+		this.content = await newContentPromise;
+		console.log('valor dps do await newContent = ', newContentPromise)
+		console.log('valor dps do await this.content = ', this.content)
+	}
 }
