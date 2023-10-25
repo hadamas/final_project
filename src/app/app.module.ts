faf8f1751f215ebbbf10cddeb579e919ed3a3289
Ítalo Marcelo <youngitne@gmail.com>
2023-10-25 12:54:40 -0300

    Criando os Componentes de Educção e Experiência

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EducacaoComponent } from './components/educacao/educacao.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducacaoComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule
