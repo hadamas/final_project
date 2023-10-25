07ba9335d5441e1ae8537cebaa65358bc16fb28b
Ítalo Marcelo <youngitne@gmail.com>
2023-10-25 17:40:44 -0300

    Modificando Estilos e Adicionando Imagens

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./experiencia.component.css']
 })
 export class ExperienciaComponent {
-
+  experiencia = {
+    text: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, consequuntur! In ea velit culpa aut sit ad odio magni error nihil, quidem ipsa ipsum. Praesentium modi vero temporibus pariatur ut.`,
+    ano: '2019-2023'
+  }
 }
