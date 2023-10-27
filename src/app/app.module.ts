f7e2e9aabfa1d04aa788aafe599257147b071657
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-10-26 23:58:54 -0300

    Fix 'app.module' to support 'educacao' and 'experiencia' components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { ProfileTabComponent } from './components/profile-tab/profile-tab.component';
 import { CurriculumHeaderComponent } from './components/curriculum-header/curriculum-header.component';
 import { CurriculumSummarySectionComponent } from './components/curriculum-summary-section/curriculum-summary-section.component';
+import { EducacaoComponent } from './components/educacao/educacao.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		ProfileTabComponent,
 		CurriculumHeaderComponent,
-		CurriculumSummarySectionComponent
+		CurriculumSummarySectionComponent,
+		EducacaoComponent,
+		ExperienciaComponent
 	],
 	imports: [
 		BrowserModule
